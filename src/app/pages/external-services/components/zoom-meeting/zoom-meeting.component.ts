16687f15e1ea42ba5ba827da25a5023423f78c29
eddzhizhpon <eddzhizhpon@gmail.com>
2021-08-07 00:18:14 -0500

    feat: Manejo de sesiones para Zoom Meeting

--- src/app/pages/external-services/components/zoom-meeting/zoom-meeting.component.ts
+++ src/app/pages/external-services/components/zoom-meeting/zoom-meeting.component.ts
@@ -1,5 +1,8 @@
+/* eslint-disable quote-props */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component, Input, OnInit } from '@angular/core';
 import { Zoom } from '@ionic-native/zoom/ngx';
+import { ToastController } from '@ionic/angular';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -9,49 +12,62 @@ import { environment } from 'src/environments/environment';
 })
 export class ZoomMeetingComponent implements OnInit {
 
-  // @Input() meetingNumber: string;
-  // @Input() meetingPassword: string;
-  // @Input() displayName: string;
+  @Input() meetingType: string;
+  @Input() displayName: string;
+
+  @Input() meetingNumber: string;
+  @Input() meetingPassword: string;
 
   userName: string;
   password: string;
-  meetingNumber: string;
-  meetingPassword: string;
-  displayName: string;
+
+  isLogged: boolean;
 
   options: any = {
-    "no_driving_mode":true,
-    "no_invite":true,
-    "no_meeting_end_message":true,
-    "no_titlebar":false,
-    "no_bottom_toolbar":false,
-    "no_dial_in_via_phone":true,
-    "no_dial_out_to_phone":true,
-    "no_disconnect_audio":true,
-    "no_share":true,
-    "no_audio":true,
-    "no_video":true,
-    "no_meeting_error_message":true
+    'no_driving_mode':true,
+    'no_invite':true,
+    'no_meeting_end_message':true,
+    'no_titlebar':false,
+    'no_bottom_toolbar':false,
+    'no_dial_in_via_phone':true,
+    'no_dial_out_to_phone':true,
+    'no_disconnect_audio':true,
+    'no_share':true,
+    'no_audio':true,
+    'no_video':true,
+    'no_meeting_error_message':true
   };
 
-  constructor(private zoomService: Zoom) {
-    this.zoomService.initialize(environment.ZOOM_SDK_KEY, environment.ZOOM_SDK_SECRET)
-      .then((success: any) => console.log(success))
-      .catch((error: any) => console.log(error));
+  constructor(private zoomService: Zoom,
+      private toastController: ToastController) {
+    // this.zoomService.initialize(environment.ZOOM_SDK_KEY, environment.ZOOM_SDK_SECRET)
+    //   .then((success: any) => console.log(success))
+    //   .catch((error: any) => console.log(error));
+
+      this.isLogged = false;
   }
 
   ngOnInit() {
+    this.isLoggedIn();
   }
 
   login() {
     this.zoomService.login(this.userName, this.password)
-      .then((success: any) => console.log(success))
-      .catch((error: any) => console.log(error));
+      .then((success: any) => {
+        this.isLogged = true;
+        this.showToast('Inicio de sesión en Zoom correcto' + JSON.stringify(success));
+        console.log(success);
+      })
+      .catch((error: any) => {
+        this.isLogged = false;
+        this.showToast('No se pudo iniciar sesión. Verifique que sus datos sean correctos');
+        console.log(error);
+      });
   }
 
   logout(){
     this.zoomService.logout()
-      .then((success: boolean) => console.log(success))
+      .then((success: boolean) => this.isLogged = false)
       .catch((error: any) => console.log(error));
   }
 
@@ -65,6 +81,19 @@ export class ZoomMeetingComponent implements OnInit {
     this.zoomService.joinMeeting(this.meetingNumber, this.meetingPassword, this.displayName, this.options)
       .then((success: any) => console.log(success))
       .catch((error: any) => console.log(error));
+  }
+
+  isLoggedIn() {
+    this.zoomService.isLoggedIn()
+    .then((success: boolean) => this.isLogged = success)
+    .catch((error: any) => this.isLogged = false);
+  }
+
+  showToast(msg: string) {
+    this.toastController.create({
+      message: msg,
+      duration: 4000
+    }).then(toast => toast.present());
   }
 
 }
