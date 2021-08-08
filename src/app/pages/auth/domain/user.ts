1c0501bbab7e3fc72ef4765fbb8cf5bf3df69108
Bryan Sarmiento <51723446+bryansb@users.noreply.github.com>
2021-08-08 00:15:28 -0500

    feat: Registro mediante email y contraseña

---
+++ src/app/pages/auth/domain/user.ts
@@ -0,0 +1,17 @@
+export class User {
+    uid: string;
+    userType: string;
+    email: string;
+    password: string;
+    profilePhoto: any;
+    name: string;
+    lastname: string;
+    birthday: string;
+    phoneNumber: string;
+    weight: number;
+    height: number;
+    homeLatitude: number;
+    homeLongitude: number;
+    zoomIdMeeting: string;
+    zoomPasswordMeeting: string;
+}
--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ToastController } from '@ionic/angular';
+
+import { User } from '../domain/user';
+import { AuthenticationService } from '../services/authentication.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -9,9 +14,41 @@ export class RegisterPage implements OnInit {
 
   selectRole: any;
 
-  constructor() { }
+  user: User;
+
+  constructor(private authService: AuthenticationService,
+    private toastController: ToastController) { }
 
   ngOnInit() {
+    this.user = new User();
+  }
+
+  userTypeChanged(ev: any) {
+    console.log('Type changed', ev.detail.value);
+    this.user.userType = ev.detail.value;
+  }
+
+  registerUserEmail(){
+    console.log(this.user);
+    if (this.user.userType != null){
+      this.authService.signupUser(this.user).then( (data) => {
+        console.log(data);
+      }).catch( (reason: any) => {
+        const msg = '[ERROR]: ' + JSON.stringify(reason);
+        const colorCode = 'danger';
+        this.showToast(msg, colorCode);
+      });
+    } else {
+      this.showToast('Elija primero el tipo de usuario', 'danger');
+    }
+  }
+
+  showToast(msg: string, colorCode: string) {
+    this.toastController.create({
+      message: msg,
+      duration: 2000,
+      color: colorCode
+    }).then(toast => toast.present());
   }
 
 }
